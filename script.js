83d8aa82fc343e6f9c82d6c8b3790c6fbf22e18e
Jason <123558105+JChow999@users.noreply.github.com>
2023-11-23 16:42:57 -0500

    Adjust html to for interface. Adjust css to improve layout. Adjust js to improve game logic and win conditions.

---
+++ script.js
@@ -0,0 +1,71 @@
+
+function Gameboard() {
+    let board = ["", "", "", 
+                "", "", "",
+                "", "", ""]
+
+    const win_conditions = [[1, 2, 3]
+    ]
+
+    function resetBoard() {
+        board = ["", "", "", 
+        "", "", "",
+        "", "", ""]
+    }
+}
+
+winningConditions = [
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+let board = ["X", "O", "X", 
+            "O", "X", "X",
+            "X", "O", ""]
+            
+for (n = 0; n < 3; n++) {
+    let tempBoard = [];
+    tempBoard.push(n);
+    while (tempBoard.length < 3) {
+        tempBoard.push(tempBoard[tempBoard.length-1] + 3)
+    }
+    winningConditions.push(tempBoard)
+}
+
+
+for (n = 0; n < 7; n += 3) {
+    let tempBoard = [];
+    tempBoard.push(n);
+    while (tempBoard.length < 3) {
+        tempBoard.push(tempBoard[tempBoard.length-1] + 1)
+    }
+    winningConditions.push(tempBoard)
+}
+console.log(winningConditions)
+
+// function checkWinner() {
+//     let symbol = "X"
+//     let playerSpots = [];
+//     for (n = 0; n < 9; n++) {
+//         if (board[n] == symbol) {
+//             playerSpots.push(n)
+//         }
+//     }
+
+//     // Check winning conditions
+//     for (n = 0; n < winningConditions.length; n++) {
+//         console.log(winningConditions[n])
+//         let accumulator = 0
+//         for (m = 0; m < winningConditions[n].length; m++) {
+//             if (playerSpots.includes(winningConditions[n][m])) {
+//                 accumulator++
+//             }
+//             console.log(accumulator)
+//         }
+//         if (accumulator >= 3) return "Winner"
+//     }
+
+//     return playerSpots;
+// }
+
+// console.log(checkWinner())
